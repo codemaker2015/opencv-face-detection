696883fd7d85ec398b03c1d7526ad0951d67ccfd
Vishnu <codemaker2015@gmail.com>
2022-11-20 17:33:50 +0530

    initial commit

---
+++ face_detection.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+while True:
+    _, img = cap.read()
+    img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+cap.release()
